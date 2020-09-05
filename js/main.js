c1dc34a7c4e9d7c9d57603b36014b61f994254e0
Viktor <without.hands95@gmail.com>
2020-09-05 21:04:02 +0300

    created section agents with slider

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+  $('.agents-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow: '.slider-arrow__prev',
+    nextArrow: '.slider-arrow__next'
+  });
+
+});
