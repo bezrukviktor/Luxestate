63a33f893e8e2dc9c87b741f3db8a8085cf781e8
Viktor <without.hands95@gmail.com>
2020-09-06 20:58:50 +0300

    created adaptive for landing

--- js/main.js
+++ js/main.js
@@ -5,17 +5,30 @@ $(document).ready(function () {
     $('.menu__nav').toggleClass('menu__nav--active');
   });
 
-  $('.counter__num').each(function(){
-    $(this).prop('Counter', 0).animate({
-      Counter: $(this).text()
-    }, {
-      duration: 3000,
-      easing: 'swing',
-      step: function(now){
-        $(this).text(Math.ceil(now));
-      }
-    });
+
+  const target = $('.counter');
+  let targetPos = target.offset().top;
+  let winHeight = $(window).height();
+  let scrollToElem = targetPos - winHeight + 300;
+
+  $(window).scroll(function () {
+    let winScrollTop = $(this).scrollTop();
+    if (winScrollTop > scrollToElem) {
+      $('.counter__num').each(function () {
+        $(this).prop('Counter', 0).animate({
+          Counter: $(this).data('value')
+        }, {
+          duration: 3000,
+          easing: 'swing',
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          }
+        });
+      });
+    }
   });
+
+
 
   $('.agents-slider').slick({
     slidesToShow: 3,
@@ -23,7 +36,34 @@ $(document).ready(function () {
     autoplay: true,
     autoplaySpeed: 4000,
     prevArrow: '.slider-arrow__prev',
-    nextArrow: '.slider-arrow__next'
+    nextArrow: '.slider-arrow__next',
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
+
+  function windowSize() {
+    if ($(window).width() < '476') {
+      $('.footer-column__title').click(function (event) {
+        $('.footer-column ul').not($(this).next()).slideUp(300);
+        $('.footer-column__title').not($(this)).removeClass('footer-column__title--active');
+        $(this).toggleClass('footer-column__title--active').next().slideToggle(300);
+      });
+    }
+  }
+
+  $(window).load(windowSize);
 
 });
