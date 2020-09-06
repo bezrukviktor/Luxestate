d973bf91ee9fbeebba0dde6f9ee4dd98401ab018
Viktor <without.hands95@gmail.com>
2020-09-06 08:39:45 +0300

    created footer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
 
+  $('.counter__num').each(function(){
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 3000,
+      easing: 'swing',
+      step: function(now){
+        $(this).text(Math.ceil(now));
+      }
+    });
+  });
+
   $('.agents-slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
